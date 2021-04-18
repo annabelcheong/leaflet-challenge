063fe80fe73398be85a66ecf2c305737cb4c5805
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-18 21:29:15 +0800

    started on leaflet-challenge logic.js. Attempted to extract data

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,23 +10,7 @@
 
 
 
-//// CREATE A MAP ////
 
-// Creating map object
-var myMap = L.map("map-id", {
-    center: [40.7, -73.95],
-    zoom: 11
-  });
-
-// Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
 
 
 //// DATA EXTRACTION FOR MARKERS ////
@@ -35,6 +19,114 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 url = d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(response) {
 
 console.log(response);
+
+// In Geojson, run function createFeatures. 
+d3.json(url, function(data) {
+    createFeatures(data.features);
+});
+
+// Define function createFeatures. 
+// *This is used to run for each feature in the features array.
+function createFeatures(earthquakeData) {
+
+    // Give each feature a popup describing earthquake place and magnitude 
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place + 
+            "</h3> <hr> <h2> Mag:" + feature.properties.mag + "</h2>");
+    };
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData,{
+        onEachFeature: onEachFeature
+    });
+
+    // Run function createMap 
+    createMap(earthquakes);
+
+};
+
+function createMap(earthquakes){
+
+    // Creating map object
+    var myMap = L.map("map-id", {
+        center: [40.7, -73.95],
+        zoom: 11
+    });
+
+    // Adding tile layer to the map
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    }).addTo(myMap);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// geojson = L.circle(data,{
+
+//     // Define what  property in the features to use. Use "mag" (which is the magnitude)
+//     valueProperty: "mag";
+
+
+// });
+
+
+//       // Create a new marker cluster group
+//   var markers = L.markerClusterGroup();
+
+//   for (var i = 0; i < countries.length; i++) {
+
+//     // Conditionals for countries points
+//     var color = "";
+//     if (countries[i].points > 200) {
+//       color = "yellow";
+//     }
+//     else if (countries[i].points > 100) {
+//       color = "blue";
+//     }
+//     else if (countries[i].points > 90) {
+//       color = "green";
+//     }
+//     else {
+//       color = "red";
+//     }
+  
+//     // Add circles to map
+//     L.circle(countries[i].location, {
+//       fillOpacity: 0.75,
+//       color: "white",
+//       fillColor: color,
+//       // Adjust radius
+//       radius: countries[i].points * 1500
+//     }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+//   }
 
 
 
