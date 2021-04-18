6e731227c3d9230020cce2ba61cca9a0450d82c2
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-18 21:56:59 +0800

    started creating function createMap

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,13 +16,12 @@
 //// DATA EXTRACTION FOR MARKERS ////
 
 // Perform an API call from USGS website (earthquakes from the past 7 days)
-url = d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(response) {
-
-console.log(response);
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // In Geojson, run function createFeatures. 
 d3.json(url, function(data) {
     createFeatures(data.features);
+    console.log(data);
 });
 
 // Define function createFeatures. 
@@ -42,30 +41,32 @@ function createFeatures(earthquakeData) {
     });
 
     // Run function createMap 
-    createMap(earthquakes);
+    // createMap(earthquakes);
 
 };
 
 function createMap(earthquakes){
 
-    // Creating map object
-    var myMap = L.map("map-id", {
-        center: [40.7, -73.95],
-        zoom: 11
-    });
-
     // Adding tile layer to the map
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-    }).addTo(myMap);
+    });
+    // }).addTo(myMap);
 
 
+    // Creating map object
+    var myMap = L.map("map-id", {
+        center: [40.7, -73.95],
+        zoom: 5,
+        layers: [lightMap, earthquakes]
+    });
 
+   
 }
 
 
@@ -87,58 +88,44 @@ function createMap(earthquakes){
 
 
 
+// // geojson = L.circle(data,{
+
+// //     // Define what  property in the features to use. Use "mag" (which is the magnitude)
+// //     valueProperty: "mag";
 
 
-// geojson = L.circle(data,{
-
-//     // Define what  property in the features to use. Use "mag" (which is the magnitude)
-//     valueProperty: "mag";
+// // });
 
 
-// });
+// //       // Create a new marker cluster group
+// //   var markers = L.markerClusterGroup();
 
+// //   for (var i = 0; i < countries.length; i++) {
 
-//       // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
-
-//   for (var i = 0; i < countries.length; i++) {
-
-//     // Conditionals for countries points
-//     var color = "";
-//     if (countries[i].points > 200) {
-//       color = "yellow";
-//     }
-//     else if (countries[i].points > 100) {
-//       color = "blue";
-//     }
-//     else if (countries[i].points > 90) {
-//       color = "green";
-//     }
-//     else {
-//       color = "red";
-//     }
+// //     // Conditionals for countries points
+// //     var color = "";
+// //     if (countries[i].points > 200) {
+// //       color = "yellow";
+// //     }
+// //     else if (countries[i].points > 100) {
+// //       color = "blue";
+// //     }
+// //     else if (countries[i].points > 90) {
+// //       color = "green";
+// //     }
+// //     else {
+// //       color = "red";
+// //     }
   
-//     // Add circles to map
-//     L.circle(countries[i].location, {
-//       fillOpacity: 0.75,
-//       color: "white",
-//       fillColor: color,
-//       // Adjust radius
-//       radius: countries[i].points * 1500
-//     }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
-//   }
-
-
-
-});
-
-
-
-
-
-
-
-
+// //     // Add circles to map
+// //     L.circle(countries[i].location, {
+// //       fillOpacity: 0.75,
+// //       color: "white",
+// //       fillColor: color,
+// //       // Adjust radius
+// //       radius: countries[i].points * 1500
+// //     }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+// //   }
 
 
 
