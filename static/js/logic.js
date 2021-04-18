3c86a8dfb517fb3cad36a173163a6214532bd245
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-18 19:54:52 +0800

    created base leaflet map and called in API geojson

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,9 +7,38 @@
 // - Include popups that provide additional information about the earthquake when a marker is clicked.
 // - Create a legend that will provide context for your map data.
 
-// Perform an API call from USGS website
-earthQ = d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")
-console.log(earthQ);
+
+
+
+//// CREATE A MAP ////
+
+// Creating map object
+var myMap = L.map("map-id", {
+    center: [40.7, -73.95],
+    zoom: 11
+  });
+
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
+//// DATA EXTRACTION FOR MARKERS ////
+
+// Perform an API call from USGS website (earthquakes from the past 7 days)
+url = d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(response) {
+
+console.log(response);
+
+
+
+});
 
 
 
@@ -26,5 +55,5 @@ console.log(earthQ);
 
 
 
-///// Create map /////
+
 
