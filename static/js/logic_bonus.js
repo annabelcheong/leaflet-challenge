ea170dfdf318a7b7f2b214d9d281f419b0025668
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-23 11:10:00 +0800

    added objectives at top of page on logic_bonus.js to complete commentary

--- static/js/logic_bonus.js
+++ static/js/logic_bonus.js
@@ -1,4 +1,4 @@
-// Level 1: Basic Visualization
+// Level 1: Basic Visualization //
 // 1. Get the dataset from API https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php 
 // and for e.g., use All Earthquakes from the Past 7 Days
 // 2. Import & Visualize the Data
@@ -6,6 +6,13 @@
 // - Your data markers should reflect the magnitude of the earthquake in their size and color. Earthquakes with higher magnitudes should appear larger and darker in color.
 // - Include popups that provide additional information about the earthquake when a marker is clicked.
 // - Create a legend that will provide context for your map data.
+
+// Level 2: More Data (Optional) //
+// 1. Plot a second data set on our map.
+// 2. Add a number of base maps to choose from.
+// 3. Separate out our two different data sets into overlays that can be turned on and off independently.
+// 4. Add layer controls to our map.
+// Techtonic Plates JSON is provided at https://github.com/fraxen/tectonicplates.
 
 
 // Perform an API call from USGS website (earthquakes from the past 7 days)
